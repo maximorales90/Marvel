970fa4902e42bf70e5fa041038d76f3bf31e8055
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-03-15 00:14:46 -0300

    MVVW
    Arquitectura del Proyecto en MVVM

---
+++ Marvel/Credenciales.swift
@@ -0,0 +1,11 @@
+//
+//  Credenciales.swift
+//  Marvel
+//
+//  Created by Maximiliano Morales on 15/03/2022.
+//
+
+import SwiftUI
+
+let privateKey = "62e163c483a7ebc127ae82380ee69b15"
+let pubicKey = "4a713e705a83dfb7feebb80f8390cdfd7b085ff5"
